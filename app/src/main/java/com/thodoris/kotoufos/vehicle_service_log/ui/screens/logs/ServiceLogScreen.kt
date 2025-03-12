d8dd6662c80896c23efeaeea808019c1d3aacbd4
Buffito <thodoriskotoufos@hotmail.com>
2025-03-12 22:52:50 +0200

    Extended the search on vehicles and added one to the logs.

--- app/src/main/java/com/thodoris/kotoufos/vehicle_service_log/ui/screens/logs/ServiceLogScreen.kt
+++ app/src/main/java/com/thodoris/kotoufos/vehicle_service_log/ui/screens/logs/ServiceLogScreen.kt
@@ -1,36 +1,40 @@
 package com.thodoris.kotoufos.vehicle_service_log.ui.screens.logs
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,6 +43,7 @@ import com.thodoris.kotoufos.vehicle_service_log.ui.components.ServiceLogItem
 import com.thodoris.kotoufos.vehicle_service_log.ui.viewmodel.ServiceLogViewModel
 import com.thodoris.kotoufos.vehicle_service_log.ui.viewmodel.VehicleViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ServiceLogScreen(
     vehicleId: Int,
@@ -60,7 +65,19 @@ fun ServiceLogScreen(
     val vehicle by vehicleViewModel.vehicleById(vehicleId).collectAsState(initial = null)
     val vehicleText = "${vehicle?.make ?: ""} ${vehicle?.model ?: ""}"
 
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { }, floatingActionButton = {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        TopAppBar(title = { Text("Service Log for $vehicleText") }, actions = {
+            IconButton(onClick = {
+                showSearch = !showSearch
+                if (!showSearch) searchQuery = ""
+            }) {
+                Icon(
+                    imageVector = if (showSearch) Icons.Default.Close else Icons.Default.Search,
+                    contentDescription = if (showSearch) "Close Search" else "Search"
+                )
+            }
+        })
+    }, floatingActionButton = {
         FloatingActionButton(
             onClick = { navController.navigate("service/-1/${vehicleId}") },
             containerColor = MaterialTheme.colorScheme.primary,
@@ -76,35 +93,40 @@ fun ServiceLogScreen(
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                if (showSearch) {
-                    TextField(value = searchQuery,
-                        onValueChange = { searchQuery = it },
-                        modifier = Modifier.fillMaxWidth(),
-                        placeholder = { Text("Search Log") },
-                        trailingIcon = {
-                            Icon(imageVector = Icons.Default.Close,
-                                contentDescription = "Close",
-                                modifier = Modifier.clickable {
-                                    showSearch = false; searchQuery = ""
-                                })
-                        })
-                } else {
-                    Text(
-                        text = "Service Log for $vehicleText",
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-
-                    Icon(imageVector = Icons.Default.Search,
-                        contentDescription = "Search Icon",
-                        modifier = Modifier
-                            .size(32.dp)
-                            .clickable { showSearch = true })
+            if (showSearch) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .border(
+                            1.dp, Color.Gray, RoundedCornerShape(8.dp)
+                        )
+                        .padding(16.dp)
+                ) {
+                    Column {
+                        TextField(
+                            value = searchQuery,
+                            onValueChange = { searchQuery = it },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.White, shape = RoundedCornerShape(16.dp)),
+                            placeholder = { Text("Search Log") },
+                            trailingIcon = {
+                                Icon(imageVector = Icons.Default.Close,
+                                    contentDescription = "Close",
+                                    modifier = Modifier.clickable {
+                                        showSearch = false
+                                        searchQuery = ""
+                                    })
+                            },
+                            shape = RoundedCornerShape(16.dp),
+                            colors = TextFieldDefaults.colors(
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent
+                            )
+                        )
+                    }
                 }
             }
 
@@ -121,10 +143,8 @@ fun ServiceLogScreen(
                             fontSize = 16.sp,
                             color = Color.Gray,
                             textAlign = TextAlign.Center
-
                         )
                     }
-
                     return@LazyColumn
                 }
 
@@ -146,7 +166,6 @@ fun ServiceLogScreen(
                     )
                 }
             }
-
         }
     })
 }
--- app/src/main/java/com/thodoris/kotoufos/vehicle_service_log/ui/screens/vehicles/VehiclesScreen.kt
+++ app/src/main/java/com/thodoris/kotoufos/vehicle_service_log/ui/screens/vehicles/VehiclesScreen.kt
@@ -1,26 +1,36 @@
 package com.thodoris.kotoufos.vehicle_service_log.ui.screens.vehicles
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -30,7 +40,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,20 +47,41 @@ import androidx.navigation.NavHostController
 import com.thodoris.kotoufos.vehicle_service_log.ui.components.VehicleItem
 import com.thodoris.kotoufos.vehicle_service_log.ui.viewmodel.VehicleViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VehiclesScreen(vehicleViewModel: VehicleViewModel, navController: NavHostController) {
     val vehicles by vehicleViewModel.allVehicles.collectAsState(emptyList())
     vehicleViewModel.addDefaultVehicleTypes()
+
     var showSearch by remember { mutableStateOf(false) }
     var searchQuery by remember { mutableStateOf("") }
+    var showActiveOnly by remember { mutableStateOf(false) }
 
-    val filteredVehicles = vehicles.filter {
-        it.make.contains(searchQuery, ignoreCase = true) || it.model.contains(
-            searchQuery, ignoreCase = true
-        ) || it.licencePlate.contains(searchQuery, ignoreCase = true)
+    val filteredVehicles = vehicles.filter { vehicle ->
+        val matchesSearch =
+            vehicle.make.contains(searchQuery, ignoreCase = true) || vehicle.model.contains(
+                searchQuery,
+                ignoreCase = true
+            ) || vehicle.licencePlate.contains(searchQuery, ignoreCase = true)
+
+        val matchesActiveFilter = !showActiveOnly || vehicle.active
+
+        matchesSearch && matchesActiveFilter
     }
 
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = { }, floatingActionButton = {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        TopAppBar(title = { Text("Vehicle List") }, actions = {
+            IconButton(onClick = {
+                showSearch = !showSearch
+                if (!showSearch) searchQuery = ""
+            }) {
+                Icon(
+                    imageVector = if (showSearch) Icons.Default.Close else Icons.Default.Search,
+                    contentDescription = if (showSearch) "Close Search" else "Search"
+                )
+            }
+        })
+    }, floatingActionButton = {
         FloatingActionButton(
             onClick = { navController.navigate("vehicle/-1") },
             containerColor = MaterialTheme.colorScheme.primary,
@@ -64,37 +94,55 @@ fun VehiclesScreen(vehicleViewModel: VehicleViewModel, navController: NavHostCon
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp)
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                if (showSearch) {
-                    TextField(value = searchQuery,
-                        onValueChange = { searchQuery = it },
-                        modifier = Modifier.fillMaxWidth(),
-                        placeholder = { Text("Search Vehicles") },
-                        trailingIcon = {
-                            Icon(imageVector = Icons.Default.Close,
-                                contentDescription = "Close",
-                                modifier = Modifier.clickable {
-                                    showSearch = false; searchQuery = ""
-                                })
-                        })
+            if (showSearch) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(8.dp))
+                        .padding(16.dp)
+                ) {
+                    Column {
+                        TextField(
+                            value = searchQuery,
+                            onValueChange = { searchQuery = it },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.White, shape = RoundedCornerShape(16.dp)),
+                            placeholder = { Text("Search Vehicles") },
+                            trailingIcon = {
+                                Icon(imageVector = Icons.Default.Close,
+                                    contentDescription = "Close",
+                                    modifier = Modifier.clickable {
+                                        showSearch = false
+                                        searchQuery = ""
+                                    })
+                            },
+                            shape = RoundedCornerShape(16.dp),
+                            colors = TextFieldDefaults.colors(
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent
+                            )
+                        )
 
-                } else {
-                    Text(
-                        text = "Vehicle List", fontSize = 20.sp, fontWeight = FontWeight.Bold
-                    )
+                        Spacer(modifier = Modifier.height(8.dp))
 
-                    Icon(imageVector = Icons.Default.Search,
-                        contentDescription = "Search Icon",
-                        modifier = Modifier
-                            .size(32.dp)
-                            .clickable { showSearch = true })
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Show Active Vehicles", modifier = Modifier.weight(1f)
+                            )
+                            Switch(checked = showActiveOnly, onCheckedChange = { isChecked ->
+                                showActiveOnly = isChecked
+                            })
+                        }
+                    }
                 }
             }
 
@@ -113,7 +161,6 @@ fun VehiclesScreen(vehicleViewModel: VehicleViewModel, navController: NavHostCon
                             textAlign = TextAlign.Center
                         )
                     }
-
                     return@LazyColumn
                 }
 
@@ -135,8 +182,7 @@ fun VehiclesScreen(vehicleViewModel: VehicleViewModel, navController: NavHostCon
                     )
                 }
             }
-
-
         }
     })
 }
+
